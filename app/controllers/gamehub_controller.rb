91939706e316e70750620b51f2b6329708ec785d
UrielAngeles09 <uriel.angeles01@utrgv.edu>
2025-04-14 02:33:12 +0000

    Game Details page added

--- app/controllers/gamehub_controller.rb
+++ app/controllers/gamehub_controller.rb
@@ -10,6 +10,10 @@ class GamehubController < ApplicationController
     redirect_to home_path unless authenticated?
   end
 
+  def gamedetails
+
+  end
+
   def dashboard
     redirect_to new_session_path unless authenticated?
   end
