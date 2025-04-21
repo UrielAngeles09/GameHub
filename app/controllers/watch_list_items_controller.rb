92e2b29e0a9e9a61079f42619e96c2ee2a3390c3
angel6arredondo <143659558+angel6arredondo@users.noreply.github.com>
2025-04-21 02:16:51 +0000

    Fix class name in WatchListItemsController

--- app/controllers/watch_list_items_controller.rb
+++ app/controllers/watch_list_items_controller.rb
@@ -1,14 +1,15 @@
-def create
+class WatchListItemsController < ApplicationController
+  def create
     @game = Game.find(params[:game_id])
     @watch_list_item = current_user.watch_list_items.create(game: @game)
-  
-    # Create a notification when a game is added to the watch list
+
     Notification.create!(
       user: current_user,
       title: "You've added #{ @game.name } to your watch list!",
       body: "We'll notify you about future updates for this game.",
       read: false
     )
-  
+
     redirect_to watch_list_path(current_user), notice: "#{@game.name} added to your watch list!"
   end
+end
